4a27df96d4a74200610999c67c66311472366811
Alex Aitken <alex@alexaitken.com>
2012-02-21 20:56:45 -0500

    push down the regualer movie rental price  to the sub class

--- src/com/refactoring/RegularPrice.java
+++ src/com/refactoring/RegularPrice.java
@@ -6,5 +6,14 @@ public class RegularPrice extends Price {
 	public int getPriceCode() {
 		return Movie.REGULAR;
 	}
+
+	@Override
+	public double getCharge(int daysRented) {
+		double result = 0;
+		result += 2;
+		if (daysRented > 2) 
+			result += (daysRented - 2) * 1.5;
+		return result;
+	}
 	
 }
