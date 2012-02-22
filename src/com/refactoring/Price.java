5d8d9e1ad80f90fc826f56b36b295e8377c83277
Alex Aitken <alex@alexaitken.com>
2012-02-21 20:57:40 -0500

    remove unneed code from the price class for regular rentals

--- src/com/refactoring/Price.java
+++ src/com/refactoring/Price.java
@@ -6,11 +6,6 @@ public abstract class Price {
 	public double getCharge(int daysRented) {
 		double result = 0;
 		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2) 
-				result += (daysRented - 2) * 1.5;
-			break;
 		case Movie.NEW_RELEASE:
 			result += daysRented * 3;
 			break;
