4170b9d8bd5f57a81277de3474d0d993ca361773
Alex Aitken <alex@alexaitken.com>
2012-02-21 21:02:08 -0500

    pushed down childrens movie charges to sub class and make get charge abstract

--- src/com/refactoring/ChildrensPrice.java
+++ src/com/refactoring/ChildrensPrice.java
@@ -6,5 +6,15 @@ public class ChildrensPrice extends Price {
 	public int getPriceCode() {
 		return Movie.CHILDRENS;
 	}
+
+	@Override
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
+		return result;
+	}
+	
+	
 	
 }
--- src/com/refactoring/Price.java
+++ src/com/refactoring/Price.java
@@ -3,17 +3,6 @@ package com.refactoring;
 public abstract class Price {
 	public abstract int getPriceCode();
 
-	public double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
-	}
-
+	public abstract double getCharge(int daysRented);
 }
 
