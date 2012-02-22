6ed413432421edbc52c35129feb89ab3560c25ed
Alex Aitken <alex@alexaitken.com>
2012-02-21 20:49:46 -0500

    add price stragies to the Movie class and replace the price code variable.

--- src/com/refactoring/Movie.java
+++ src/com/refactoring/Movie.java
@@ -9,7 +9,7 @@ public class Movie {
 	public static final int REGULAR = 0;
 	
 	private String _title;
-	private int _priceCode;
+	private Price _price;
 	
 	public Movie(String title, int priceCode) {
 		_title = title;
@@ -17,11 +17,23 @@ public class Movie {
 	}
 	
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect price code!");
+		}
 	}
 	public String getTitle() {
 		return _title;
