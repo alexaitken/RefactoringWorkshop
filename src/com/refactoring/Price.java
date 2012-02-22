88cd46efd849dc3a9096e54d6ade36d6faabca90
Alex Aitken <alex@alexaitken.com>
2012-02-21 21:08:55 -0500

    pushed down the change in frequent renter points for New releases to the new release price class.

--- src/com/refactoring/Price.java
+++ src/com/refactoring/Price.java
@@ -6,9 +6,6 @@ public abstract class Price {
 	public abstract double getCharge(int daysRented);
 
 	public int getFrequentRenterPoints(int daysRented) {
-		if (getPriceCode() == Movie.NEW_RELEASE) 
-			return (daysRented > 1) ? 2 : 1;
-		
 		return 1;
 	}
 }
