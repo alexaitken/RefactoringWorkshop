853fc072b37a2f10d091a8595db8ec37892e85a1
Alex Aitken <alex@alexaitken.com>
2012-02-21 19:53:03 -0500

    moved from temprary variable for the query method for the charge on each rental.

--- src/com/refactoring/Customer.java
+++ src/com/refactoring/Customer.java
@@ -34,8 +34,6 @@ public class Customer {
 			
 			Rental each = rentals.next();
 			
-			double thisAmount = each.getCharge();
-			
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -43,8 +41,8 @@ public class Customer {
 				frequentRenterPoints++;
 			
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
 		}
 		
 		// add footer lines
